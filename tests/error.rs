939ec00eb4959ef2e766e3943d5593a989c43040
yukimemi <yukimemi@gmail.com>
2019-03-03 14:45:53 +0900

    :bug: Fix tests.

--- tests/error.rs
+++ tests/error.rs
@@ -2,15 +2,15 @@ extern crate checksums;
 
 use checksums::Error;
 
-
 #[test]
 fn exit_value() {
     assert_eq!(Error::NoError.exit_value(), 0);
     assert_eq!(Error::OptionParsingError.exit_value(), 1);
     assert_eq!(Error::HashLengthDiffers.exit_value(), 2);
     assert_eq!(Error::HashesFileParsingFailure.exit_value(), 3);
-    assert_eq!(Error::NFilesDiffer(1).exit_value(), 4);
-    assert_eq!(Error::NFilesDiffer(10).exit_value(), 13);
+    assert_eq!(Error::DifferentNumberOfFiles.exit_value(), 4);
+    assert_eq!(Error::NFilesDiffer(1).exit_value(), 5);
+    assert_eq!(Error::NFilesDiffer(10).exit_value(), 14);
 }
 
 #[test]
@@ -19,6 +19,7 @@ fn from_i32() {
     assert_eq!(Error::from(1), Error::OptionParsingError);
     assert_eq!(Error::from(2), Error::HashLengthDiffers);
     assert_eq!(Error::from(3), Error::HashesFileParsingFailure);
-    assert_eq!(Error::from(4), Error::NFilesDiffer(1));
-    assert_eq!(Error::from(13), Error::NFilesDiffer(10));
+    assert_eq!(Error::from(4), Error::DifferentNumberOfFiles);
+    assert_eq!(Error::from(5), Error::NFilesDiffer(1));
+    assert_eq!(Error::from(14), Error::NFilesDiffer(10));
 }
